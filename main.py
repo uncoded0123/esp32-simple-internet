45f28f2e84bfd8cfc5f871f4e225a2df7903e7b5
Shannon Cody Russell <48037883+uncoded0123@users.noreply.github.com>
2023-06-10 14:58:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import urequests, http1
 response = urequests.get("<website_url>")
-print(response) # response.<whatever> (eg, response.json())
+print(response) # response.<whatever> (eg, response.text, or response.json())
 response.close()
