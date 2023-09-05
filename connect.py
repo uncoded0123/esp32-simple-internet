31152eec1583dd7e19b58dbe751730378a0d1aeb
Shannon Cody Russell <48037883+uncoded0123@users.noreply.github.com>
2023-09-05 02:10:46 -0500

    Update and rename http1.py to connect.py

--- connect.py
+++ connect.py
@@ -2,6 +2,6 @@ import time, network
 station = network.WLAN(network.STA_IF)
 station.active(True)
 while station.isconnected() == False:
-    station.connect("<SSID", "Password")
+    station.connect("SSID", "Password")
     time.sleep(1)
 print("Connected!")
